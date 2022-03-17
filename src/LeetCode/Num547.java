a6f2e2a36da4954525c3f3da79d8af5975f779ef
angrebobo <1172176452@qq.com>
2022-03-17 16:08:48 +0800

    547

--- src/LeetCode/Num547.java
+++ src/LeetCode/Num547.java
@@ -6,38 +6,43 @@ package LeetCode;
  * @Data: Created in 10:52 2022/3/17
  */
 public class Num547 {
+
+    //题解1，深度优先搜索
     static class Solution {
-        boolean[][] visited;
+        boolean[] visited;
         int num = 0;
+        int len;
         public int findCircleNum(int[][] isConnected) {
-            int len = isConnected.length;
-            visited = new boolean[len][len];
+            len = isConnected.length;
+            visited = new boolean[len];
             for (int i = 0; i < len; i++) {
-                for (int j = 0; j < len; j++) {
-                    visited[i][j] = false;
-                }
+                visited[i] = false;
             }
 
             for (int i = 0; i < len; i++) {
-                for (int j = i+1; j < len; j++) {
-                    if(isConnected[i][j] == 1 && !visited[i][j]){
-                        DFS(isConnected, i ,j);
-                        num++;
-                    }
+                //如果该城市还未被访问，去DFS中递归访问与该城市连结的城市
+                if(!visited[i]){
+                    DFS(isConnected, i);
+                    num++;
                 }
             }
 
             return num;
         }
 
-        public void DFS(int[][] isConnected, int i, int j){
-
+        public void DFS(int[][] isConnected,int i){
+            for (int j = 0; j < len; j++) {
+                if(isConnected[i][j]==1 && !visited[j]){
+                    visited[j] = true;
+                    DFS(isConnected, j);
+                }
+            }
         }
     }
 
     public static void main(String[] args) {
-        int[][] isConnected = {{1,1,0},
-                               {1,1,0},
+        int[][] isConnected = {{1,0,0},
+                               {0,1,0},
                                {0,0,1}};
 
         Solution solution = new Solution();
