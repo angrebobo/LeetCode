71f2d587aa9341d5a87819921627ae6d2b71a520
angrebobo <1172176452@qq.com>
2022-03-30 22:10:43 +0800

    215

--- src/LeetCode/Num215.java
+++ src/LeetCode/Num215.java
@@ -8,10 +8,10 @@ package LeetCode;
 public class Num215 {
 
     /* 解法1，调用库函数 */
-//    public static int findKthLargest(int[] nums, int k) {
-//        Arrays.sort(nums);
-//        return nums[nums.length - k];
-//    }
+    /*public static int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }*/
 
 
     //解法2，利用快排的特性，每次partion后都会有一个元素到达它的最终位置
@@ -44,7 +44,7 @@ public class Num215 {
         nums[RandomStart] = temp;
 
         int pivot = nums[left];
-        while (left < right){
+         while (left < right){
             while (left < right && nums[right] >= pivot){
                 right--;
             }
@@ -95,14 +95,57 @@ public class Num215 {
         }
     }
 
+    //2022.03.20写
+    public static int findKthLargest(int[] nums, int k){
+        int left = 0;
+        int right = nums.length-1;
+        int target = nums.length-k;
+
+        while (true){
+            int index = qucikSort(nums, left, right);
+            if(index == target)
+                return nums[index];
+            else if(index < target){
+                left = index+1;
+            }
+            else {
+                right = index-1;
+            }
+        }
+    }
+
+    public static int qucikSort(int[] nums, int left, int right){
+        //使序列变得无序
+        int randomStart = (int) (left + Math.random()*(right-left));
+        int temp = nums[left];
+        nums[left] = nums[randomStart];
+        nums[randomStart] = temp;
+
+        int pivot = nums[left];
+
+        while (left < right){
+            while (left < right && nums[right] > pivot){
+                right--;
+            }
+            nums[left] = nums[right];
+            while (left < right && nums[left] < pivot){
+                left++;
+            }
+            nums[right] = nums[left];
+        }
+
+        nums[left] = pivot;
+        return left;
+    }
+
 
     public static void main(String[] args) {
 
         int[] input = {5,2,4,1,3,6,0};
         int k = 4;
         Solution solution = new Solution();
-        System.out.println(solution.findKthLargest(input, k));
+//        System.out.println(solution.findKthLargest(input, k));
 
-//        System.out.println(findKthLargest(input, k));
+        System.out.println( findKthLargest(input, k) );
     }
 }
--- src/LeetCode/Test.java
+++ src/LeetCode/Test.java
@@ -1,8 +1,6 @@
 package LeetCode;
 
 
-import java.util.Arrays;
-
 /**
  * @Description TODO
  * @Author HuangSiBo
@@ -11,38 +9,7 @@ import java.util.Arrays;
 public class Test {
 
     public static void main(String[] args) {
-//        String tagId = "31231231231231232";
-//        if(substringBefore(tagId, "_").length() >= 10){
-//            System.out.println("true");
-//        }
-//        else
-//            System.out.println("false");
-//        String s = "dasd";
-//        char[] chars = s.toCharArray();
-//        Arrays.sort(chars);
-//        System.out.println(String.valueOf(chars));
-        //StringBuffer buffer = new StringBuffer(String.valueOf(chars));
 
-        String s = "abcdefgh";
-        System.out.println( s.substring(0,8) );
-    }
-
-    public static String substringBefore(final String str, final String separator) {
-        if (isEmpty(str) || separator == null) {
-            return str;
-        }
-        if (separator.isEmpty()) {
-            return "";
-        }
-        final int pos = str.indexOf(separator);
-        if (pos == -1) {
-            return str;
-        }
-        return str.substring(0, pos);
-    }
-
-    public static boolean isEmpty(final CharSequence cs) {
-        return cs == null || cs.length() == 0;
     }
 }
 
