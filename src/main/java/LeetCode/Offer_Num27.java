935e1478e7d124805fb3df817bcf40614601b36a
angrebobo <1172176452@qq.com>
2022-05-23 16:02:03 +0800

    镜像二叉树

--- src/main/java/LeetCode/Offer_Num27.java
+++ src/main/java/LeetCode/Offer_Num27.java
@@ -6,11 +6,38 @@ package LeetCode;
  * @Data: Created in 15:20 2022/5/23
  */
 public class Offer_Num27 {
-    class Solution {
-        public TreeNode mirrorTree(TreeNode root) {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) { this.val = val; }
+    }
 
+    static class Solution {
+        public TreeNode mirrorTree(TreeNode root) {
+            if(root == null)
+                return null;
+
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+
+            mirrorTree(root.left);
+
+            mirrorTree(root.right);
+
+            return root;
         }
     }
 
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        n1.left = n2;
+        n1.right = n3;
 
+        Solution solution = new Solution();
+        solution.mirrorTree(n1);
+    }
 }
