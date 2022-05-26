2896350326e0d9a71f6a1ba45475ce964f61daff
bobo <1172176452@qq.com>
2022-05-27 00:08:41 +0800

    N皇后

--- src/main/java/LeetCode/Num51.java
+++ src/main/java/LeetCode/Num51.java
@@ -12,6 +12,7 @@ public class Num51 {
     static class Solution {
         public List<List<String>> solveNQueens(int n) {
             List<List<String>> res = new LinkedList<>();
+            //record记录皇后的位置，record[i]表示第i个皇后放在第record[i]列
             int[] record = new int[n];
             List<String> path = new LinkedList<>();
             backTrace(0, record, n, res, path);
@@ -24,16 +25,17 @@ public class Num51 {
                 res.add(new LinkedList<>(path));
 
             for (int j = 0; j < n; j++) {
-                if( isValue(i, j, record) ){
+                if( isValid(i, j, record) ){
                     record[i] = j;
 
-                    path.add(process(j, n));
+                    path.add( process(j, n) );
                     backTrace(i+1, record, n, res, path);
                     path.remove(path.size()-1);
                 }
             }
         }
 
+        //按照题目格式要求打印
         public String process(int j, int n){
             StringBuffer buffer = new StringBuffer();
             for (int k = 0; k < n; k++) {
@@ -49,7 +51,7 @@ public class Num51 {
         /**
          * 检测在第i行第j列放置皇后，是否会和已放置的皇后同列同斜线
          */
-        public boolean isValue(int i, int j, int[] record){
+        public boolean isValid(int i, int j, int[] record){
             for (int k = 0; k < i; k++) {
                 if(j == record[k] || Math.abs(k-i)==Math.abs(record[k]-j))
                     return false;
