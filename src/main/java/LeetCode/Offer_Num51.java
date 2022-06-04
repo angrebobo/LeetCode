0a58082afc03a5f4d9a795d3616649063543197b
bobo <1172176452@qq.com>
2022-06-05 00:34:21 +0800

    逆序对

--- src/main/java/LeetCode/Offer_Num51.java
+++ src/main/java/LeetCode/Offer_Num51.java
@@ -7,32 +7,35 @@ package LeetCode;
  */
 public class Offer_Num51 {
     static class Solution {
+
         int reversePairsCount;
 
         //用归并排序
         public int reversePairs(int[] nums) {
             reversePairsCount = 0;
-            mergeSort(nums, 0, nums.length-1);
+            mergeSort(nums, 0, nums.length-1, new int[nums.length]);
             return reversePairsCount;
         }
 
-        public void mergeSort(int[] nums, int left, int right){
+        public void mergeSort(int[] nums, int left, int right, int[] temp){
             if(left < right){
                 int mid = left + (right-left)/2;
-                mergeSort(nums, left, mid);
-                mergeSort(nums, mid+1, right);
-                merge(nums, left, mid, mid+1, right);
+                mergeSort(nums, left, mid, temp);
+                mergeSort(nums, mid+1, right, temp);
+                merge(nums, left, mid, mid+1, right, temp);
             }
         }
 
-        public void merge(int[] nums, int left1, int right1, int left2, int right2){
-            int[] temp = new int[nums.length];
+        public void merge(int[] nums, int left1, int right1, int left2, int right2, int[] temp){
             int i = left1;
             int j = left2;
             int k = 0;
 
             while (i <= right1 && j <= right2){
-                if (nums[i] < nums[j])
+                // 注意！！！这里的等号要加上。
+                // 如果只是写归并排序，这个等号加或不加结果不会变
+                // 但是现在是nums[i] > nums[j]，才能去计算逆序对的值，所以等号要加上
+                if (nums[i] <= nums[j])
                     temp[k++] = nums[i++];
                 else {
                     temp[k++] = nums[j++];
@@ -47,7 +50,6 @@ public class Offer_Num51 {
                 nums[left1+l] = temp[l];
             }
         }
-
     }
 
     public static void main(String[] args) {
