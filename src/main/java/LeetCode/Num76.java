d8b2fd97bdced9632dcf97359b6267d51674ebd5
angrebobo <1172176452@qq.com>
2022-07-03 20:27:15 +0800

    79.单词搜索

--- src/main/java/LeetCode/Num76.java
+++ src/main/java/LeetCode/Num76.java
@@ -60,28 +60,28 @@ public class Num76 {
             return true;
         }*/
 
+        // 方法2，滑动窗口。用int数组替代hashmap，运行时间大大缩短
         public String minWindow(String s, String t){
+            // map: 子串还需要的字符
             int[] map = new int[128];
+            // need: 统计t的字符，need数组不做改动
             int[] need = new int[128];
             for (int i = 0; i < t.length(); i++) {
                 map[t.charAt(i)-'0'] += 1;
                 need[t.charAt(i)-'0'] += 1;
             }
             int res_len = Integer.MAX_VALUE;
-            int res_start=-1;
+            int res_start=-1, res_end=-1;
             int start=0, end=0;
-            int count = t.length();
             while (end < s.length()){
                 // 使end往右移动，直到子串符合要求
-                while (count>0 && end<s.length()){
+                while (!isComplete(map) && end<s.length()){
                     char c = s.charAt(end);
-                    if(need[c-'0'] > 0){
+                    if(need[c-'0'] > 0)
                         map[c-'0'] -= 1;
-                        count--;
-                    }
                     end++;
                 }
-                if(count > 0)
+                if(!isComplete(map))
                     break;
                 // 使start也往右移动，排除无用元素，直到s.charAt(start)是所需字母
                 while (start <= end-1){
@@ -91,11 +91,11 @@ public class Num76 {
                             // 符合的子串下标是start -> end-1
                             if(end-start < res_len){
                                 res_start = start;
-                                res_len = end-res_start;
+                                res_end = end-1;
+                                res_len = res_end-res_start+1;
                             }
                             start++;
                             map[c-'0'] += 1;
-                            count++;
                             break;
                         }
                         else if(map[c-'0'] < 0)
@@ -105,7 +105,15 @@ public class Num76 {
                 }
             }
 
-            return (res_start>=0)?(s.substring(res_start, res_start+res_len)):("");
+            return (res_start>=0)?(s.substring(res_start, res_end+1)):("");
+        }
+
+        public boolean isComplete(int[] map){
+            for (int i = 0; i < 128; i++) {
+                if(map[i] > 0)
+                    return false;
+            }
+            return true;
         }
     }
 
