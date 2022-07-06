c59af2a2aebd73dfa18553d268064195f297ff97
angrebobo <1172176452@qq.com>
2022-07-06 21:48:26 +0800

    402

--- src/main/java/LeetCode/Num402.java
+++ src/main/java/LeetCode/Num402.java
@@ -5,21 +5,26 @@ import java.util.*;
 /**
  * @author: HuangSiBo
  * @Description:
+ * 题解：https://leetcode.cn/problems/remove-k-digits/solution/wei-tu-jie-dan-diao-zhan-dai-ma-jing-jian-402-yi-d/
  * @Data: Created in 19:01 2022/7/4
  */
 public class Num402 {
     static class Solution {
+        //单调栈
         public String removeKdigits(String num, int k) {
             Deque<Character> deque = new LinkedList<>();
             for(Character c : num.toCharArray()){
+                // 维持一个递增的单调栈，去掉高位大的数字
                 while (k>0 && !deque.isEmpty() && c<deque.peekLast()){
                     deque.removeLast();
                     k--;
                 }
+                // 删除前导0
                 if(c!='0' || !deque.isEmpty())
                     deque.addLast(c);
             }
 
+            // 若k个数字没删够，删掉低位，因为低位数字大
             while (k>0 && !deque.isEmpty()){
                 deque.pollLast();
                 k--;
