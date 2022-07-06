863935f505b46a6ab34266d49b9d442cb72dd76e
angrebobo <1172176452@qq.com>
2022-07-06 20:55:13 +0800

    48旋转矩阵

--- src/main/java/LeetCode/Num48.java
+++ src/main/java/LeetCode/Num48.java
@@ -26,8 +26,18 @@ public class Num48 {
             }
         }*/
 
+        //方法2，原地修改
         public void rotate(int[][] matrix){
-
+            int len = matrix.length;
+            for (int i = 0; i < len/2; i++) {
+                for (int j = 0; j < (len+1)/2; j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[len-j-1][i];
+                    matrix[len-j-1][i] = matrix[len-i-1][len-j-1];
+                    matrix[len-i-1][len-j-1] = matrix[j][len-i-1];
+                    matrix[j][len-i-1] = temp;
+                }
+            }
         }
     }
 
