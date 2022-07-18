82969aafb51bef2aba1cd2cb4df5bd8c62916aa1
angrebobo <1172176452@qq.com>
2022-07-18 20:16:00 +0800

    字典序

--- src/main/java/LeetCode/Num440.java
+++ src/main/java/LeetCode/Num440.java
@@ -1,16 +1,16 @@
 package LeetCode;
 
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * @author: HuangSiBo
  * @Description:
+ * 题解：https://leetcode.cn/problems/k-th-smallest-in-lexicographical-order/solution/ben-ti-shi-shang-zui-wan-zheng-ju-ti-de-shou-mo-sh/
+ * https://leetcode.cn/problems/k-th-smallest-in-lexicographical-order/solution/wu-xu-jie-zhu-shi-cha-shu-ye-neng-rong-yi-li-jie-b/
+ *
  * @Data: Created in 22:45 2022/7/16
  */
 public class Num440 {
     static class Solution {
-        public int findKthNumber(int n, int k) {
+        /*public int findKthNumber(int n, int k) {
             List<String> list = new LinkedList<>();
             for (int i = 1; i <= n; i++) {
                 list.add(i + "");
@@ -32,13 +32,54 @@ public class Num440 {
             });
 
             return Integer.parseInt(list.get(k-1));
+        }*/
+
+        // 前提知识1：字典序，一个数增加1，和一个数*10，前者的字典序更大
+        // 前提知识2：十叉树
+        public int findKthNumber(int n, int k) {
+            int prefix=1;
+            int p=1;
+            while(p<k){
+                int cnt=get_count(prefix,n);
+                // 以prefix开头的数字串太多
+                // 更新成prefix*10，即在十叉树中往下了一层
+                // prefix更新成prefix*10后，其字典序增加了1
+                if(p+cnt > k){
+                    prefix *= 10;
+                    p++;
+                }
+                // 以prefix开头的数字串不够
+                // prefix加1
+                // 其字典序增加cnt
+                else if(p+cnt <= k){
+                    prefix++;
+                    p += cnt;
+                }
+            }
+            return prefix;
+        }
+
+        /**
+         * get_count函数的作用是求出，以prefix为前缀的，且不超过n的数字个数
+         * 思路就是从位数出发，比如prefix=45， n=4566
+         * 先考虑两位数的情况，结果是45,
+         * 三位数的情况，结果是450~459
+         * 四位数的情况，结果是4500~Math.min(4566，4599)
+         */
+        public int get_count(int prefix,int n){
+            int cnt=0;
+            for(int a=prefix,b=prefix+1; a<=n; a*=10,b*=10){
+                cnt += Math.min(n+1,b) - a;
+            }
+            return cnt;
         }
     }
 
     public static void main(String[] args) {
-        int n = 1;
-        int k = 1;
+        int n = 2042;
+        int k = 45;
         Solution solution = new Solution();
         System.out.println(solution.findKthNumber(n, k));
+//        System.out.println(solution.get_count(1, 12));
     }
 }
