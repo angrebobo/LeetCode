18b801797c0a247691c00a780ced24b1e210b4e0
bobo <1172176452@qq.com>
2022-08-23 21:09:35 +0800

    143.重排列表

--- src/main/java/LeetCode/Num143.java
+++ src/main/java/LeetCode/Num143.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.ArrayList;
+
 /**
  * @desiption:
  * @author: HuangSiBo
@@ -34,7 +36,21 @@ public class Num143 {
     }*/
 
     public static void reorderList(ListNode head) {
+        ArrayList<ListNode> list = new ArrayList<>();
+        ListNode p = head;
+        while (p != null){
+            list.add(p);
+            p = p.next;
+        }
 
+        int len = list.size();
+        for (int i = 0; i < len/2; i++) {
+            list.get(i).next = list.get(len-1-i);
+            if(i+1<len-1-i)
+                list.get(len-1-i).next = list.get(i+1);
+        }
+
+        list.get(len/2).next = null;
     }
 
     public static void main(String[] args) {
@@ -42,8 +58,14 @@ public class Num143 {
         ListNode n2 = new ListNode(2);
         ListNode n3 = new ListNode(3);
         ListNode n4 = new ListNode(4);
-        n1.next = n2; n2.next = n3; /*n3.next = n4;*/
+//        ListNode n5 = new ListNode(5);
+        n1.next = n2; n2.next = n3; n3.next = n4;/*n4.next = n5;*/
         reorderList(n1);
+        ListNode p = n1;
+        while (p != null){
+            System.out.println(p.val);
+            p = p.next;
+        }
     }
 
 
