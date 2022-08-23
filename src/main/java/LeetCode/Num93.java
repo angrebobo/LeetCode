12d2fc0375b9b900f9d5353e722b78fa936e9dde
bobo <1172176452@qq.com>
2022-08-23 20:00:01 +0800

    93. 复原 IP 地址

--- src/main/java/LeetCode/Num93.java
+++ src/main/java/LeetCode/Num93.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,71 +11,57 @@ import java.util.List;
  * @Data: Created in 19:11 2021/8/3
  */
 public class Num93 {
-
     static class Solution {
         public List<String> restoreIpAddresses(String s) {
-            List<String> res = new LinkedList<>();
-            int len = s.length();
-            //ip地址最短长度为4，最长长度为12
-            if(len < 4 || len > 12)
-                return res;
+            List<String> ans = new ArrayList<>();
+            Deque<String> path = new LinkedList<>();
+            if(s.length()<4)
+                return ans;
 
-            List<String> ip = new LinkedList<>();
-            dfs(0, 0, ip, len, res, s);
-            return res;
+            backTrace(0, 0, s, path, ans);
+            return ans;
         }
 
-        /**
-         *
-         * @param splitTimes 当前分段次数
-         * @param begin 从下标begin处开始分段
-         * @param ip
-         * @param len
-         * @param res
-         * @param s
-         */
-        public void dfs(int splitTimes, int begin, List<String> ip, int len, List<String> res, String s){
-            //当分段次数达到4次并且
-            if(splitTimes==4 && begin==len)
-                res.add( String.join(".", ip) );
+        //回溯题，其实就是3种选择，选1个字符还是2个字符还是3个字符
+        public void backTrace(int splitTimes, int index, String s,
+                              Deque<String> path, List<String> ans){
 
-            //当剩余长度过短或者过长，停止分段
-            if(len-begin < 4-splitTimes || len-begin > 3*(4-splitTimes))
-                return;
-
-            for (int i = 0; i < 3; i++) {
-                if(begin + i >= len)
-                    break;
-
-                int isValid = isValidIpAddress(s, begin, begin+i);
-                if(isValid != -1){
-                    ip.add(isValid + "");
-                    dfs(splitTimes+1, begin+i+1, ip, len, res, s);
-                    ip.remove(ip.size()-1);
+                // splitTimes代表切分次数
+                if(splitTimes == 4 && index == s.length()){
+                    ans.add(String.join(".", path));
                 }
-            }
+
+                // 当前切分得太短
+                if(index+3*(4-splitTimes) < s.length()-1)
+                    return;
+                // 当前切分得太长
+                if(s.length()-index< 4-splitTimes)
+                    return;
+
+                // 回溯的过程，3种长度
+                for(int i = 1; i <= 3; i++) {
+                    if(index+i > s.length())
+                        break;
+                    else {
+                        String temp = s.substring(index, index + i);
+                        if(isVal(temp)) {
+                            path.addLast(temp);
+                            backTrace(splitTimes + 1, index + i, s, path, ans);
+                            path.removeLast();
+                        }
+                    }
+                }
         }
 
-        /**
-         * 判断s的[begin，end]分段是否是一个合法分段
-         * @param s
-         * @param begin
-         * @param end
-         * @return
-         */
-        public int isValidIpAddress(String s, int begin, int end){
-            int len = end - begin + 1;
-            //不能以0开头
-            if(len > 1 && s.charAt(begin)=='0')
-                return -1;
-
-            String temp = s.substring(begin, end+1);
-            int res = Integer.parseInt(temp);
-            //不能大于255
-            if(res > 255)
-                return -1;
-
-            return res;
+        // 判断选中的字符串是不是合格的ip地址
+        public boolean isVal(String s){
+            int len = s.length();
+            // 不能有前导0
+            if(len>1 && s.charAt(0)=='0')
+                return false;
+            int v = Integer.parseInt(s);
+            // 不能大于255
+            return v <= 255;
         }
     }
 
