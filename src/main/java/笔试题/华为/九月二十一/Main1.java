67e3430c0dbf4380901abf74847f44e483c2406e
bobo <1172176452@qq.com>
2022-09-21 23:50:38 +0800

    华为笔试

--- src/main/java/笔试题/华为/九月二十一/Main1.java
+++ src/main/java/笔试题/华为/九月二十一/Main1.java
@@ -1,3 +1,5 @@
+package 笔试题.华为.九月二十一;
+
 import java.util.HashMap;
 import java.util.Scanner;
 
--- src/main/java/笔试题/华为/九月二十一/Main2.java
+++ src/main/java/笔试题/华为/九月二十一/Main2.java
@@ -1,3 +1,5 @@
+package 笔试题.华为.九月二十一;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
